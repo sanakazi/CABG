50a3d57455cbf03b0d8a792d85e9c128e435cedb
sanakazi <kazisana23@gmail.com>
2016-12-09 11:00:00 +0530

    commit

---
+++ app/src/main/java/com/example/sanakazi/cabg/others/AboutUs.java
@@ -0,0 +1,120 @@
+package com.example.sanakazi.cabg.others;
+
+import android.content.Context;
+
+import com.example.sanakazi.cabg.R;
+
+/**
+ * Created by SanaKazi on 12/5/2016.
+ */
+public class AboutUs {
+
+   public static Context context;
+
+    public AboutUs(Context context) {
+        this.context = context;
+    }
+
+    public static String intro = "Edwards Lifesciences is the global leader in patient-focused medical innovations for structural heart disease, as well as critical care and surgical monitoring. Driven by a passion to help patients, the company collaborates with the world’s leading clinicians and researchers to address unmet healthcare needs, working to improve patient outcomes and enhance lives.";
+    public static String history ="Edwards Lifesciences’ roots date to 1958, when Miles “Lowell” Edwards set out to build the first artificial heart.\n" +
+            "\n" +
+            "Edwards was a 60-year-old, recently retired engineer holding 63 patents in an array of industries, with an entrepreneurial spirit and a dream of helping patients with heart disease. His fascination with healing the heart was sparked in his teens, when he suffered two bouts of rheumatic fever, which can scar heart valves and eventually cause the heart to fail.\n" +
+            "\n" +
+            "With a background in hydraulics and fuel pump operations, Edwards believed the human heart could be mechanized. He presented the concept to Dr. Albert Starr, a young surgeon at the University of Oregon Medical School, who thought the idea was too complex. Instead, Starr encouraged Edwards to focus first on developing an artificial heart valve, for which there was an immediate need.\n" +
+            "\n" +
+            "After just two years, the first Starr-Edwards mitral valve was designed, developed, tested, and successfully placed in a patient. Newspapers around the world reported on what they termed a “miraculous” heart surgery.\n" +
+            "\n" +
+            "This innovation spawned a company, Edwards Laboratories, which set up shop in Santa Ana, California – not far from where Edwards Lifesciences’ corporate headquarters is located today.\n" +
+            "\n" +
+            "In 1966, Edwards Laboratories was purchased by American Hospital Supply Corporation and became American Edwards Laboratories. Then, in 1985, American Edwards was acquired by Baxter International Inc. In early 2000, the company was spun-off as an independent, publicly-held corporation and began trading on the New York Stock Exchange under the symbol “EW.”\n" +
+            "\n" +
+            "Our spirit of innovation remains central to Edwards today. The number of patients requiring treatment for cardiovascular disease and critical illnesses is increasing dramatically, and further growth is expected in the future. Several factors are feeding this dynamic: an aging population, high incidence of rheumatic fever in developing nations, and improved diagnostic techniques that allow physicians to detect problems sooner.\n" +
+            "\n" +
+            "Today, we continue to lead the field of tissue replacement heart valves and repair products and advanced hemodynamic monitoring, which have helped treat and manage more than 2 million patients worldwide.\n" +
+            "\n" +
+            "With more than 50 years of experience, we have grown into a global company, with a presence in approximately 100 countries and approximately 8,500 employees around the world. Each one of them is dedicated to furthering Edwards’ original vision to help clinicians, patients and their families work together as a united community fighting cardiovascular disease and critical illnesses.\n";
+
+
+    public static String what_we_do = "Edwards Lifesciences is the global leader in patient-focused medical innovations for structural heart disease, as well as critical care and surgical monitoring. Driven by a passion to help patients, the company collaborates with the world’s leading clinicians and researchers to address unmet healthcare needs, working to improve patient outcomes and enhance lives.\n" +
+            "\n" +
+            "<b>"+"Heart Valve Technologies" + "</b>" +"\n" +
+            "\n" +
+            "Dedicated to improving the lives of patients with structural heart disease, our commitment to transformational heart valve technology began in 1960 with the first commercially available heart valve. Edwards has continued to meaningfully advance heart valve therapy, and, more than 50 years later, we remain a global leader in this field. Our offerings include tissue replacement heart valves and repair products, as well as transcatheter heart valves for those patients considered at high or greater risk for conventional valve replacement.\n" +
+            "\n" +
+            "The Edwards' proven family of PERIMOUNT surgical valves are the world's most frequently implanted valves. Decades of clinical experience and peer-reviewed data on Edwards' valves provide robust evidence of long-term durability and proven hemodynamics of the PERIMOUNT valve platform.\n" +
+            "\n" +
+            "Edwards’ leadership in transcatheter heart valve replacement includes a commitment to meaningful innovation, rigorous scientific study, extensive clinician training and education, and significant investment in new applications of the technology. The Edwards SAPIEN valve platform continues to raise the bar as the most widely studied transcatheter heart valve worldwide.\n" +
+            "\n" +
+            "Edwards also offers market-leading technologies that facilitate on-pump cardiac surgery procedures through smaller incisions. Edwards’ minimally invasive valve surgery solutions are comprised of soft tissue retractors, venous and arterial cannulae, aortic occlusion, venting, and coronary sinus catheters.\n" +
+            "\n" +
+            "<b>"+"Critical Care Technologies\n"+ "</b>" +
+            "\n" +
+            "Edwards has been advancing the care of the acutely ill for more than 40 years. Starting with the development of Swan-Ganz advanced hemodynamic monitoring devices for measuring cardiovascular performance in high-risk surgical and critically ill patients, Edwards continues to evolve to less-invasive and noninvasive devices to provide valuable hemodynamic insight for an expanded patient population. Edwards has continued to advance hemodynamic monitoring platforms to enable proactive clinical decisions for surgical, anesthesia and critical care clinicians. Edwards is focused on helping clinicians enhance knowledge and standardize practice to improve the quality and efficiency of care. Edwards provides evidence-based programs such as Enhanced Surgical Recovery Program that support the implementation and compliance to protocolized care pathways in the OR and ICU.\n" +
+            "\n" +
+            "Our vascular therapies products, including genuine Fogarty catheters, continue this high standard. Developed and refined in close collaboration with surgeons and interventionalists for over 50 years, Fogarty catheters have been used in millions of procedures worldwide.\n";
+
+
+
+    public static String our_credo = "At Edwards Lifesciences, we are dedicated to providing innovative solutions for people fighting cardiovascular disease.\n" +
+            "\n" +
+            "Through our actions, we will become trusted partners with customers, colleagues, and patients - creating a community unified in its mission to improve the quality of life around the world. Our results will benefit customers, patients, employees, and shareholders.\n" +
+            "\n" +
+            "We will celebrate our successes, thrive on discover, and continually expand our boundaries. We will act boldly, decisively, and with determination on behalf of people fighting cardiovascular disease.\n" +
+            "\n" +
+            "Helping Patients is Our Life’s Work, and Life is Now.\n";
+public static String disclaimer = "" +
+        "Patients diagnosed with coronary disease requiring coronary artery bypass graft surgery (CABG) may also have mild to moderate aortic stenosis (AS) . Co-presentation with both coronary and valve disease is increasing as the age of patients referred for CABG surgery rises. If AS is severe  or the patient has symptoms, aortic valve replacement (AVR) should be performed in conjunction with CABG surgery . There is controversy, however, over the appropriate care of asymptomatic patients with mild or moderate stenosis . If no intervention is done at the time of CABG, AS symptoms may develop, necessitating a second open-heart procedure (AVR), with additional technical challenges and complications . Conversely, an initial CABG/AVR increases the initial operative risk and exposes patients to potential long-term valve-related complications . Numerous clinical factors, including severity and rate of progression of AS, patient life expectancy, and probability of valve- or operative-related complications, must be considered in making this decision.\n" +
+        " \n" +
+        "This app utilizes multiple clinical studies  to assess the relative benefits of prophylactic AVR. Multiple sensitivity analyses are also performed to determine the variables that most profoundly affect outcome, and to recommend treatment thresholds.\n" +
+        " \n" +
+        "This app provides a decision aid for treating patients with mild to moderate AS requiring CABG surgery. Predictors of AS progression will differ in patients depending on  various factors ( body type/ age / severity of disease etc ) .There is no definite predictor of the rate of progression , this app summarizes multiple data points and evidence based literature to give the clinician a guide in predicting the progression of AS and also comparing the complications associated with CABG alone vis –a-vis CABG+ AVR or AVR at a later stage.\n" +
+        " \n" +
+        "The CABG+AVR Evaluator App  is intended to provide guidance to physicians and patients who are faced with a difficult clinical decision regarding replacement of a stenotic aortic valve during coronary surgery. Three major factors affect the decision to perform CABG/AVR or CABG alone: age (or life expectancy), baseline peak aortic valve gradient by echocardiography, and rate of AS progression. Assuming an average rate of progression, CABG/AVR should be considered once the baseline gradient exceeds 30 mm Hg. Individual patient characteristics, including comorbidities, local expertise in performing operations, and patient preference are still important when making this difficult decision.\n" +
+        " \n" +
+        " \n" +
+        "Edwards Lifesciences accept no liability for the patient outcome derived from this evaluator   which may be the result of the consequences of varied patient types , comorbid conditions and geographical variability \n";
+
+
+   public static String history_html = "Edwards Lifesciences’ roots date to 1958, when Miles “Lowell” Edwards set out to build the first artificial heart.<br><br>\n" +
+           "\n" +
+           "Edwards was a 60-year-old, recently retired engineer holding 63 patents in an array of industries, with an entrepreneurial spirit and a dream of helping patients with heart disease. His fascination with healing the heart was sparked in his teens, when he suffered two bouts of rheumatic fever, which can scar heart valves and eventually cause the heart to fail.<br><br>\n" +
+           "\n" +
+           "With a background in hydraulics and fuel pump operations, Edwards believed the human heart could be mechanized. He presented the concept to Dr. Albert Starr, a young surgeon at the University of Oregon Medical School, who thought the idea was too complex. Instead, Starr encouraged Edwards to focus first on developing an artificial heart valve, for which there was an immediate need. <br><br>\n" +
+           "\n" +
+           "After just two years, the first Starr-Edwards mitral valve was designed, developed, tested, and successfully placed in a patient. Newspapers around the world reported on what they termed a “miraculous” heart surgery.<br><br>\n" +
+           "\n" +
+           "This innovation spawned a company, Edwards Laboratories, which set up shop in Santa Ana, California – not far from where Edwards Lifesciences’ corporate headquarters is located today.<br><br>\n" +
+           "\n" +
+           "In 1966, Edwards Laboratories was purchased by American Hospital Supply Corporation and became American Edwards Laboratories. Then, in 1985, American Edwards was acquired by Baxter International Inc. In early 2000, the company was spun-off as an independent, publicly-held corporation and began trading on the New York Stock Exchange under the symbol “EW.”<br><br>\n" +
+           "\n" +
+           "Our spirit of innovation remains central to Edwards today. The number of patients requiring treatment for cardiovascular disease and critical illnesses is increasing dramatically, and further growth is expected in the future. Several factors are feeding this dynamic: an aging population, high incidence of rheumatic fever in developing nations, and improved diagnostic techniques that allow physicians to detect problems sooner.<br><br>\n" +
+           "\n" +
+           "Today, we continue to lead the field of tissue replacement heart valves and repair products and advanced hemodynamic monitoring, which have helped treat and manage more than 2 million patients worldwide.<br><br>\n" +
+           "\n" +
+           "With more than 50 years of experience, we have grown into a global company, with a presence in approximately 100 countries and approximately 8,500 employees around the world. Each one of them is dedicated to furthering Edwards’ original vision to help clinicians, patients and their families work together as a united community fighting cardiovascular disease and critical illnesses.\n";
+
+
+    public static String what_we_do_html="Edwards Lifesciences is the global leader in patient-focused medical innovations for structural heart disease, as well as critical care and surgical monitoring. Driven by a passion to help patients, the company collaborates with the world’s leading clinicians and researchers to address unmet healthcare needs, working to improve patient outcomes and enhance lives.<br><br>\n" +
+            "<b>Heart Valve Technologies</b><br><br>\n" +
+            "Dedicated to improving the lives of patients with structural heart disease, our commitment to transformational heart valve technology began in 1960 with the first commercially available heart valve. Edwards has continued to meaningfully advance heart valve therapy, and, more than 50 years later, we remain a global leader in this field. Our offerings include tissue replacement heart valves and repair products, as well as transcatheter heart valves for those patients considered at high or greater risk for conventional valve replacement.<br><br>\n" +
+            "The Edwards' proven family of PERIMOUNT surgical valves are the world's most frequently implanted valves. Decades of clinical experience and peer-reviewed data on Edwards' valves provide robust evidence of long-term durability and proven hemodynamics of the PERIMOUNT valve platform.<br><br>\n" +
+            "Edwards’ leadership in transcatheter heart valve replacement includes a commitment to meaningful innovation, rigorous scientific study, extensive clinician training and education, and significant investment in new applications of the technology. The Edwards SAPIEN valve platform continues to raise the bar as the most widely studied transcatheter heart valve worldwide.<br><br>\n" +
+            "Edwards also offers market-leading technologies that facilitate on-pump cardiac surgery procedures through smaller incisions. Edwards’ minimally invasive valve surgery solutions are comprised of soft tissue retractors, venous and arterial cannulae, aortic occlusion, venting, and coronary sinus catheters.<br><br>\n" +
+            "<b>Critical Care Technologies</b><br><br>\n" +
+            "Edwards has been advancing the care of the acutely ill for more than 40 years. Starting with the development of Swan-Ganz advanced hemodynamic monitoring devices for measuring cardiovascular performance in high-risk surgical and critically ill patients, Edwards continues to evolve to less-invasive and noninvasive devices to provide valuable hemodynamic insight for an expanded patient population. Edwards has continued to advance hemodynamic monitoring platforms to enable proactive clinical decisions for surgical, anesthesia and critical care clinicians. Edwards is focused on helping clinicians enhance knowledge and standardize practice to improve the quality and efficiency of care. Edwards provides evidence-based programs such as Enhanced Surgical Recovery Program that support the implementation and compliance to protocolized care pathways in the OR and ICU.<br><br>\n" +
+            "Our vascular therapies products, including genuine Fogarty catheters, continue this high standard. Developed and refined in close collaboration with surgeons and interventionalists for over 50 years, Fogarty catheters have been used in millions of procedures worldwide.\n";
+
+
+    public  static  String our_credo_html = "At Edwards Lifesciences, we are dedicated to providing innovative solutions for people fighting cardiovascular disease.<br><br>\n" +
+            "\n" +
+            "\n" +
+            "Through our actions, we will become trusted partners with customers, colleagues, and patients - creating a community unified in its mission to improve the quality of life around the world. Our results will benefit customers, patients, employees, and shareholders.<br><br>\n" +
+            "\n" +
+            "\n" +
+            "We will celebrate our successes, thrive on discover, and continually expand our boundaries. We will act boldly, decisively, and with determination on behalf of people fighting cardiovascular disease.<br><br>\n" +
+            "\n" +
+            "\n" +
+            "Helping Patients is Our Life’s Work, and Life is Now.<br><br>";
+
+
+}
